c5d7d5a2ca098402bbd9f839a2b9a68d5fd8a8a6
LenieBIRD-5. <leniesuetos@gmail.com.>
2026-02-02 19:44:16 -0800

    firebase practice with navigation bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import NavLogo from './assets/nav-logo.png';
+import { auth } from "./firebase/init";
+import { 
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+ } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user.email[0].toUpperCase);
+      if (user) {
+        setUser(user)
+      }
+    })
+  }, []);
+
+  function login() {
+  signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then(({ user }) => {
+      console.log(user);
+      setUser(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+  function register() {
+    console.log('register');
+    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then((user) => {
+      console.log(user)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <nav>
+            <div className="nav__container">
+                <a href="/">
+                <img src={NavLogo} alt="" className="logo" />
+                </a>
+                <ul className="nav__links">
+          <li className="nav__list">
+            <button onClick={login} className="nav__btn">
+            Login
+            </button>
+          </li>
+          <li className="nav__list">
+            <button onClick={register} className="nav__btn">
+            Register
+            </button>
+          </li>
+          <li className="nav__list">
+            <button onClick={logout} className="btn">
+              E
+            </button>
+            {loading ? 'loading...' : user.email}
+          </li>
+                </ul>
+            </div>
+        </nav>
     </div>
   );
 }
---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAI3vnzvwKdTsLd2QlSB3POs4C8Ewzr-nM",
+  authDomain: "fir-practice-361df.firebaseapp.com",
+  projectId: "fir-practice-361df",
+  storageBucket: "fir-practice-361df.firebasestorage.app",
+  messagingSenderId: "250297388064",
+  appId: "1:250297388064:web:02bb27fa327907567e8045"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
